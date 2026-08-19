0461a6f47abcc3e58b068f75c5d8f6e466f3cfd1
aserko-del <a.serko@coffee-embassy.by>
2026-08-20 01:44:43 +0300

    Update generator.py

--- generator.py
+++ generator.py
@@ -5,19 +5,42 @@ OUTPUT_FILE = "zala_auto.m3u"
 PLAYLIST_CONTENT = """#EXTM3U url-tvg="http://epg.it999.ru/epg2.xml.gz" refresh="24"
 
 #EXTINF:-1 group-title="Беларуские", Беларусь 1
-http://iptv.zala.by/stream/belarus1/index.m3u8
+https://hls.belarus24.by/b24/index.m3u8
 #EXTINF:-1 group-title="Беларуские", Беларусь 2
-http://iptv.zala.by/stream/belarus2/index.m3u8
+https://stream.voka.tv/live/belarus2/index.m3u8
 #EXTINF:-1 group-title="Беларуские", Беларусь 3
-http://iptv.zala.by/stream/belarus3/index.m3u8
+https://stream.voka.tv/live/belarus3/index.m3u8
+#EXTINF:-1 group-title="Беларуские", Беларусь 4
+https://stream.voka.tv/live/belarus4/index.m3u8
+#EXTINF:-1 group-title="Беларуские", Беларусь 5
+https://stream.voka.tv/live/belarus5/index.m3u8
 #EXTINF:-1 group-title="Беларуские", ОНТ
-http://iptv.zala.by/stream/ont/index.m3u8
+https://ont.by/hls/stream.m3u8
 #EXTINF:-1 group-title="Беларуские", СТВ
-http://iptv.zala.by/stream/stv/index.m3u8
-#EXTINF:-1 group-title="Познавательные", Discovery Channel
-http://iptv.zala.by/stream/discovery/index.m3u8
-#EXTINF:-1 group-title="Фильмы", Еврокино
-http://iptv.zala.by/stream/eurokino/index.m3u8
+https://ctv.by/hls/stream.m3u8
+#EXTINF:-1 group-title="Беларуские", МИР
+https://mir24.tv/hls/stream.m3u8
+
+#EXTINF:-1 group-title="Информационные", НТВ
+https://ntv.ru/hls/stream.m3u8
+#EXTINF:-1 group-title="Информационные", Россия 1
+https://live.russia.tv/hls/stream.m3u8
+
+#EXTINF:-1 group-title="Кино и Сериалы", Амедиа
+https://stream.voka.tv/live/amedia/index.m3u8
+#EXTINF:-1 group-title="Развлекательные", Домашний
+https://domashniy.ru/hls/stream.m3u8
+#EXTINF:-1 group-title="Кино и Сериалы", Дом Кино
+https://domkino.tv/hls/stream.m3u8
+#EXTINF:-1 group-title="Кино и Сериалы", Феникс плюс Кино
+https://fenix.tv/hls/stream.m3u8
+#EXTINF:-1 group-title="Кино и Сериалы", ТВ 1000 Русское кино
+https://stream.voka.tv/live/tv1000rk/index.m3u8
+#EXTINF:-1 group-title="Развлекательные", ТВ3
+https://tv3.ru/hls/stream.m3u8
+
+#EXTINF:-1 group-title="Музыка", Шансон TV
+https://shanson.tv/hls/stream.m3u8
 """
 
 def generate():
