7d5851e91b626c5f4811e7e3c9659a417191a0be
aserko-del <a.serko@coffee-embassy.by>
2026-08-20 01:34:09 +0300

    Update generator.py

--- generator.py
+++ generator.py
@@ -2,7 +2,6 @@ import os
 
 OUTPUT_FILE = "zala_auto.m3u"
 
-# Готовый рабочий шаблон плейлиста ZALA (расширяемый)
 PLAYLIST_CONTENT = """#EXTM3U url-tvg="http://epg.it999.ru/epg2.xml.gz" refresh="24"
 
 #EXTINF:-1 group-title="Беларуские", Беларусь 1
@@ -17,17 +16,14 @@ http://iptv.zala.by/stream/ont/index.m3u8
 http://iptv.zala.by/stream/stv/index.m3u8
 #EXTINF:-1 group-title="Познавательные", Discovery Channel
 http://iptv.zala.by/stream/discovery/index.m3u8
-#EXTINF:-1 group-title="Фильмы", еврокино
+#EXTINF:-1 group-title="Фильмы", Еврокино
 http://iptv.zala.by/stream/eurokino/index.m3u8
 """
 
 def generate():
-    try:
-        with open(OUTPUT_FILE, "w", encoding="utf-8") as f:
-            f.write(PLAYLIST_CONTENT.strip())
-        print("Плейлист успешно сформирован.")
-    except Exception as e:
-        print(f"Ошибка при сохранении: {e}")
+    with open(OUTPUT_FILE, "w", encoding="utf-8") as f:
+        f.write(PLAYLIST_CONTENT.strip())
+    print("Плейлист успешно сформирован.")
 
 if __name__ == "__main__":
     generate()
